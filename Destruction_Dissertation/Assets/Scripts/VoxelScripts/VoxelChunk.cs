0c98a1b1d11626a06cf0d2fc628ca3117b7c4adb
Seblong2 <sebastian2.long@live.uwe.ac.uk>
2025-02-17 20:25:58 +0000

    Voxel pipelines
    Added the pipelines for voxel destruction

---
+++ Destruction_Dissertation/Assets/Scripts/VoxelScripts/VoxelChunk.cs
@@ -0,0 +1,65 @@
+using System;
+using UnityEngine;
+
+public class VoxelChunk : MonoBehaviour
+{
+    public int chunkSize = 16;
+    private Voxel[,,] voxels;
+    private MeshFilter meshFilter;
+    private MeshCollider meshCollider;
+    private VoxelMeshGenerator meshGenerator;
+
+    public struct Voxel
+    {
+        public bool isActive;
+    }
+
+    void Start()
+    {
+        meshFilter = gameObject.AddComponent<MeshFilter>();
+        meshCollider = gameObject.AddComponent<MeshCollider>();
+        meshGenerator = new VoxelMeshGenerator();
+
+        voxels = new Voxel[chunkSize,chunkSize,chunkSize];
+        InitializeVoxels();
+        GenerateMesh();
+    }
+
+     void InitializeVoxels()
+    {
+       for (int x = 0; x < chunkSize; x++)
+        {
+            for(int y = 0; y < chunkSize; y++)
+            {
+                for (int z = 0; z < chunkSize; z++)
+                {
+                    voxels[x, y, z].isActive = true; //Starts all the voxels together as a solid object
+                }
+            }
+        }
+    }
+
+    public void GenerateMesh()
+    {
+        Mesh mesh = meshGenerator.GenerateMesh(voxels, chunkSize);
+        MeshFilter.mesh = mesh;
+        meshCollider.sharedMesh = mesh;
+    }
+
+    public void DestroyVoxel(Vector3Int localPosition)
+    {
+        if (localPosition.x >= 0 && localPosition.x < chunkSize &&
+            localPosition.y >= 0 && localPosition.y < chunkSize &&
+            localPosition.z >= 0 && localPosition.z < chunkSize)
+        {
+            voxels[localPosition.x, localPosition.y,localPosition.z].isActive = false;
+            GenerateMesh();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
