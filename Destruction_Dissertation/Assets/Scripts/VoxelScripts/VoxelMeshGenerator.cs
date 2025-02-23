e9f7116878a6da769b4ea437bb6302017d1150aa
Seblong2 <sebastian2.long@live.uwe.ac.uk>
2025-02-23 22:19:45 +0000

    Update Mesh Generator

--- Destruction_Dissertation/Assets/Scripts/VoxelScripts/VoxelMeshGenerator.cs
+++ Destruction_Dissertation/Assets/Scripts/VoxelScripts/VoxelMeshGenerator.cs
@@ -38,7 +38,40 @@ public class VoxelMeshGenerator : MonoBehaviour
         Vector3 pos = new Vector3(x, y, z);
         int vertIndex = vertices.Count;
 
-       
+        //Defining corners for the cube
+        Vector3[] cubeVertices = new Vector3[]
+        {
+            pos + new Vector3(0,0,0), //0
+            pos + new Vector3(1,0,0), //1
+            pos + new Vector3(1,1,0), //2
+            pos + new Vector3(0,1,0), //3
+            pos + new Vector3(0,0,1), //4
+            pos + new Vector3(1,0,1), //5
+            pos + new Vector3(1,1,1), //6
+            pos + new Vector3(0,1,1), //7
+        };
+
+        //Add Vertices
+        vertices.AddRange(cubeVertices);
+
+        //Define Faces
+        int[] cubeTriangles = new int[]
+        {
+            vertIndex, vertIndex + 1, vertIndex + 2, vertIndex, vertIndex + 2, vertIndex + 3, //FRONT
+            vertIndex + 4, vertIndex + 5, vertIndex + 6, vertIndex + 4, vertIndex + 6, vertIndex +7, //BACK
+            vertIndex + 0, vertIndex + 4, vertIndex + 7, vertIndex + 0, vertIndex + 7, vertIndex +3, //LEFT
+            vertIndex + 1, vertIndex + 5, vertIndex + 6, vertIndex + 1, vertIndex + 6, vertIndex +2, //RIGHT
+            vertIndex + 3, vertIndex + 2, vertIndex + 6, vertIndex + 3, vertIndex + 6, vertIndex +7, //TOP
+            vertIndex + 0, vertIndex + 1, vertIndex + 5, vertIndex + 0, vertIndex + 5, vertIndex +4, //BOTTOM
+        };
+
+        triangles.AddRange(cubeTriangles);
+
+        //Add uvs
+        for(int i = 0; i < 8; i++)
+        {
+            uvs.Add(new Vector2((i % 2), (i / 4)));
+        }
     }
 
 
